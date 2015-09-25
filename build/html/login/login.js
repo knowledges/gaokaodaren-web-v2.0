81902eff100d895b043776122c809c4a720b6a15
knowledges <qbl378739311@163.com>
2015-09-25 18:27:34 +0800

    add

--- build/html/login/login.js
+++ build/html/login/login.js
@@ -8,4 +8,51 @@ angular.module("gaokaoAPP.login",['ngRoute'])
         controller:'loginCtr'
     })
 }])
-.controller("loginCtr",[function(){}]);
+.controller("loginCtr",['$scope','$http',function($scope,$http){
+        $scope.user = {
+            username:"",
+            password:"",
+            validate:"",
+            remember:false
+        }
+
+        $scope.check = function(val){
+            $scope.user.remember = val;
+        }
+
+        $scope.login = function(){
+
+            var name = $scope.user.username,
+                pwd = $scope.user.password,
+                remember = $scope.user.remember;
+
+            if(name.length<=0){
+                console.log("用户名不能为空");
+                return;
+            }
+
+            if(pwd.length<=0){
+                console.log("密码不能为空");
+                return;
+            }
+            getValidCode();
+            //var URL = "/login?time="+new Date().getTime();
+
+
+            //$http.post(URL,{j_username:name,j_password:pwd,code:code})
+
+
+        }
+
+
+        function getValidCode(){
+            $http.get("/user/code?time="+new Date().getTime())
+                .success(function(data,status,headers,config){
+                    $scope.user.validate = data;
+                })
+                .error(function(data,status,headers,config){
+
+                });
+        }
+
+}]);
